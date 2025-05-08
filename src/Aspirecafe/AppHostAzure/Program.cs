2631c2d9df62ab4bcbe9a95d6e40954a491d8dbf
Robert (BuddyBear) Felkins <robert@buddynetworks.net>
2025-05-08 05:27:21 -0500

    Add connection strings for services in Program.cs
    Updated Program.cs to include connection strings for keyvault, cosmos, cache, and servicebus. Removed the previous `builder.Build().Run();` line, indicating a change in the application build and run process.

--- src/Aspirecafe/AppHostAzure/Program.cs
+++ src/Aspirecafe/AppHostAzure/Program.cs
@@ -1,15 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
-
+//resources
 var keyvault = builder.AddConnectionString("keyvault");
 var cosmos = builder.AddConnectionString("cosmos");
 var cache = builder.AddConnectionString("cache");
 var servicebus = builder.AddConnectionString("servicebus");
-
 //services
 builder.AddProject<Projects.AspireCafe_BaristaApi>("aspirecafe-baristaapi").WithReference(keyvault).WaitFor(keyvault).WithReference(cosmos).WaitFor(cosmos).WithReference(cache).WaitFor(cache).WithReference(servicebus).WaitFor(servicebus);
 builder.AddProject<Projects.AspireCafe_CounterApi>("aspirecafe-counterapi").WithReference(keyvault).WaitFor(keyvault).WithReference(cosmos).WaitFor(cosmos).WithReference(cache).WaitFor(cache).WithReference(servicebus).WaitFor(servicebus);
 builder.AddProject<Projects.AspireCafe_KitchenApi>("aspirecafe-kitchenapi").WithReference(keyvault).WaitFor(keyvault).WithReference(cosmos).WaitFor(cosmos).WithReference(cache).WaitFor(cache).WithReference(servicebus).WaitFor(servicebus);
 builder.AddProject<Projects.AspireCafe_ProductApi>("aspirecafe-productapi").WithReference(keyvault).WaitFor(keyvault).WithReference(cosmos).WaitFor(cosmos).WithReference(cache).WaitFor(cache).WithReference(servicebus).WaitFor(servicebus);
 builder.AddProject<Projects.AspireCafe_OrderSummaryApi>("aspirecafe-odersummaryapi").WithReference(keyvault).WaitFor(keyvault).WithReference(cosmos).WaitFor(cosmos).WithReference(cache).WaitFor(cache).WithReference(servicebus).WaitFor(servicebus);
-
 builder.Build().Run();
